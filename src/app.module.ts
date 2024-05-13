57e27699aee65a1e1693ceed54366c5cc7136eea
Alireza Hamid <alirezahamid20@gmail.com>
2024-05-13 23:08:36 +0100

    implement: /start and /search commands for telegram bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { PrismaService } from './prisma/prisma.service';
 import { ScraperModule } from './scraper/scraper.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { OrganizationModule } from './organization/organization.module';
+import { TelegramService } from './telegram/telegram.service';
+import { OrganizationService } from './organization/organization.service';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { OrganizationModule } from './organization/organization.module';
     OrganizationModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ScraperService, PrismaService],
+  providers: [
+    AppService,
+    ScraperService,
+    PrismaService,
+    OrganizationService,
+    TelegramService,
+  ],
 })
 export class AppModule {}
