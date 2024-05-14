b7d6a6ee87ed65830e3e1584d402893e1cb42765
Alireza Hamid <alirezahamid20@gmail.com>
2024-05-14 18:35:09 +0100

    refactor: cleaning up some functions

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -1,93 +1,52 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import axios from 'axios';
-import { parse } from 'csv-parse';
-import * as fs from 'fs';
 import { PrismaService } from 'src/prisma/prisma.service';
-import * as stream from 'stream';
-import { promisify } from 'util';
-import * as cheerio from 'cheerio';
-import { Cron } from '@nestjs/schedule';
+import { parseCSV, scrapeForCSVLink } from 'src/utils/csvParser';
+import { OrganizationRecord } from './interfaces/organization.interface';
 
 @Injectable()
-export class ScraperService {
+export class ScraperService implements OnModuleInit {
   private readonly logger = new Logger(ScraperService.name);
-  private readonly pipeline = promisify(stream.pipeline);
 
   constructor(private prisma: PrismaService) {}
 
-  @Cron('0 0 */2 * *') // Cron expression for every 2 days at midnight
+  // @Cron('0 0 */2 * *') // Cron expression for every 2 days at midnight
   async handleCron() {
     this.logger.debug('Running scheduled CSV download and processing');
     await this.downloadAndProcessCSV();
   }
 
-  // async onModuleInit() {
-  //   await this.downloadAndProcessCSV();
-  // }
-
-  async scrapeForCSVLink(): Promise<string> {
-    const url =
-      'https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers';
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    const linkElement = $(
-      'h3.gem-c-attachment__title > a.govuk-link.gem-c-attachment__link',
-    );
-
-    if (linkElement.length === 0) {
-      this.logger.error('No CSV link found');
-      throw new Error('No CSV link found');
-    }
-
-    const href = linkElement.attr('href');
-    console.log(href);
-    return href;
+  async onModuleInit() {
+    // await this.downloadAndProcessCSV();
   }
 
   async downloadAndProcessCSV() {
-    const csvUrl = await this.scrapeForCSVLink();
+    const csvUrl = await scrapeForCSVLink();
     const response = await axios({
       method: 'GET',
       url: csvUrl,
       responseType: 'stream',
     });
 
-    const path = './tmp/csv-download.csv';
-    const writer = fs.createWriteStream(path);
-
-    await this.pipeline(response.data, writer);
-
-    const records = [];
-    fs.createReadStream(path)
-      .pipe(
-        parse({
-          columns: true,
-          trim: true,
-          skip_empty_lines: true,
-          relax_quotes: true,
-        }),
-      )
-      .on('error', (err) => {
-        console.error(`Error parsing CSV: ${err.message}`);
-      })
-      .on('data', (data) => records.push(data))
-      .on('end', async () => {
-        await this.insertRecords(records);
-      });
+    const records = await parseCSV(response);
+    await this.insertRecords(records);
   }
 
-  async insertRecords(records: any[]) {
+  async insertRecords(records: OrganizationRecord[]) {
     // console.log(res);
     for (const record of records) {
+      // console.log(typeof record.county);
       const data = {
-        name: record['Organisation Name'],
-        townCity: record['Town/City'],
-        county: record['County'],
-        typeRating: record['Type & Rating'],
-        route: record['Route'],
+        name: String(record.name),
+        townCity: typeof record.townCity === 'string' ? record.townCity : '',
+        county: typeof record.county === 'string' ? record.county : '',
+        typeRating:
+          typeof record.typeRating === 'string' ? record.typeRating : '',
+        route: typeof record.route === 'string' ? record.route : '',
       };
+      // console.log(data);
       await this.prisma.organization.upsert({
-        where: { name: record['Organisation Name'] },
+        where: { name: String(record.name) },
         update: data,
         create: data,
       });
--- src/utils/csvParser.ts
+++ src/utils/csvParser.ts
@@ -1,22 +1,35 @@
+import axios from 'axios';
 import { parse } from 'csv-parse';
 import * as fs from 'fs';
-
+import * as cheerio from 'cheerio';
 import { OrganizationRecord } from 'src/scraper/interfaces/organization.interface';
+import { Logger } from '@nestjs/common';
+import { promisify } from 'util';
+import * as stream from 'stream';
 
-export async function parseCSV(
-  filePath: string,
-): Promise<OrganizationRecord[]> {
+const logger = new Logger('CsvParser');
+const pipeline = promisify(stream.pipeline);
+export async function parseCSV(response: any): Promise<OrganizationRecord[]> {
   const records: OrganizationRecord[] = [];
+  const path = './tmp/csv-download.csv';
+  const writer = fs.createWriteStream(path);
 
-  const parser = fs.createReadStream(filePath).pipe(
-    parse({
-      columns: (header) => header.map((column) => column.trim()), // This line ensures headers are trimmed of any excess whitespace
-      trim: true,
-      skip_empty_lines: true,
-      cast: true,
-      cast_date: true,
-    }),
-  );
+  await pipeline(response.data, writer);
+  const parser = fs
+    .createReadStream(path)
+    .pipe(
+      parse({
+        columns: (header) => header.map((column) => column.trim()), // This line ensures headers are trimmed of any excess whitespace
+        trim: true,
+        skip_empty_lines: true,
+        cast: true,
+        cast_date: true,
+        relax_quotes: true,
+      }),
+    )
+    .on('error', (err) => {
+      logger.error(`Error parsing CSV: ${err.message}`);
+    });
 
   for await (const record of parser) {
     records.push({
@@ -29,4 +42,22 @@ export async function parseCSV(
   }
 
   return records;
+}
+
+export async function scrapeForCSVLink(): Promise<string> {
+  const url =
+    'https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers';
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+  const linkElement = $(
+    'h3.gem-c-attachment__title > a.govuk-link.gem-c-attachment__link',
+  );
+
+  if (linkElement.length === 0) {
+    logger.error('No CSV link found');
+    throw new Error('No CSV link found');
+  }
+  const href = linkElement.attr('href');
+  logger.log(href);
+  return href;
 }
